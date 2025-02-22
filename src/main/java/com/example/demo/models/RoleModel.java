d0b8a170ef540d32849b110fa965f9033f49e062
GEAT <gersontajiboy8@gmail.com>
2025-02-22 12:00:36 -0600

    Update RoleModel.java

--- src/main/java/com/example/demo/models/RoleModel.java
+++ src/main/java/com/example/demo/models/RoleModel.java
@@ -1,9 +1,6 @@
 package com.example.demo.models;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
 
 public class RoleModel {
 
