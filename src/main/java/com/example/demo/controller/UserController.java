5201c19f7da1296c5d2c77b3fd2a098da2f03d2c
GEAT <gersontajiboy8@gmail.com>
2025-02-26 16:33:41 -0600

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,23 +2,23 @@ package com.example.demo.controller;
 
 import com.example.demo.models.RoleModel;
 import com.example.demo.models.UserModel;
+import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.util.logging.Logger;
+import java.util.List;
 
 public class UserController {
 
-    private static final Logger LOGGER = Logger.getLogger(UserController.class.getName());
+    @FXML
+    private TableView<UserModel> usersTable;
 
     @FXML
-    private TableView<UserModel> userTable;
-
-    @FXML
-    private TableColumn<UserModel, Integer> userIdColumn;
+    private TableColumn<UserModel, Integer> idColumn;
 
     @FXML
     private TableColumn<UserModel, String> usernameColumn;
@@ -35,30 +35,114 @@ public class UserController {
     @FXML
     private ComboBox<RoleModel> roleComboBox;
 
-    private ObservableList<RoleModel> rolesModelObservableList = FXCollections.observableArrayList(
-            new RoleModel(1, "ADMINISTRATOR"),
-            new RoleModel(2, "USER")
-    );
+    private ObservableList<UserModel> userModelObservableList = FXCollections.observableArrayList();
 
     @FXML
     private void initialize() {
-        roleComboBox.setItems(rolesModelObservableList);
+        // Load the roles from the database
+        List<RoleModel> roleModelList = RoleRepository.getAllRoles();
+        ObservableList<RoleModel> roleModelObservableList = FXCollections.observableList(roleModelList);
+        roleComboBox.setItems(roleModelObservableList);
+
+        // Set up table columns
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
+        roleNameColumn.setCellValueFactory(cellDataFeatures -> {
+            RoleModel roleModel = cellDataFeatures.getValue().getRole();
+            return roleModel != null ? roleModel.nameProperty() : null;
+        });
+
+        // Load the list of users
+        loadUsers();
+
+        // Listener to load data into the form when a user selects it
+        usersTable.getSelectionModel().selectedItemProperty().addListener((
+                observable,
+                oldValue,
+                newValue) -> {
+            if (newValue != null) {
+                usernameTextField.setText(newValue.getUsername());
+                passwordTextField.setText(newValue.getPassword());
+                roleComboBox.getSelectionModel().select(newValue.getRole());
+            }
+        });
     }
 
     @FXML
-    private void handleSave() {
+    private void handleCreate() {
         String username = usernameTextField.getText();
         String password = passwordTextField.getText();
         RoleModel selectedRole = roleComboBox.getSelectionModel().getSelectedItem();
-
-        if (username.isEmpty() || password.isEmpty()) {
-            LOGGER.warning("All fields are required.");
+        if (username.isEmpty() || password.isEmpty() || selectedRole == null) {
+            showAlert(Alert.AlertType.WARNING, "Validation", "All fields are required.");
         }
 
         UserModel userModel = new UserModel(0, username, password, selectedRole);
-
         UserRepository.insertUser(userModel);
+        showAlert(Alert.AlertType.INFORMATION, "User creation", "User created successfully.");
+        loadUsers();
+        clearFields();
+    }
 
-        LOGGER.info("User saved.");
+    @FXML
+    private void handleUpdate() {
+        UserModel selectedUserModel = usersTable.getSelectionModel().getSelectedItem();
+        if (selectedUserModel == null) {
+            showAlert(Alert.AlertType.WARNING, "No selection", "Please select a user to update.");
+        }
+
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+        RoleModel selectedRole = roleComboBox.getSelectionModel().getSelectedItem();
+        if (username.isEmpty() || password.isEmpty() || selectedRole == null) {
+            showAlert(Alert.AlertType.WARNING, "Validation", "All fields are required.");
+        }
+
+        selectedUserModel.setUsername(username);
+        selectedUserModel.setPassword(password);
+        selectedUserModel.setRole(selectedRole);
+        UserRepository.updateUser(selectedUserModel);
+        showAlert(Alert.AlertType.INFORMATION, "User updated", "User updated successfully.");
+        loadUsers();
+        clearFields();
+    }
+
+    @FXML
+    private void handleDelete() {
+        UserModel selectedUserModel = usersTable.getSelectionModel().getSelectedItem();
+        if (selectedUserModel == null) {
+            showAlert(Alert.AlertType.WARNING, "No selection", "Please select a user to delete");
+        }
+
+        Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationAlert.setTitle("Confirm Deletion");
+        confirmationAlert.setHeaderText(null);
+        confirmationAlert.setContentText("Are you sure you want to delete the selected user?");
+        if (confirmationAlert.showAndWait().get() == ButtonType.OK) {
+            UserRepository.deleteUser(selectedUserModel.getId());
+            showAlert(Alert.AlertType.INFORMATION, "User Deletion", "User deleted successfully.");
+            loadUsers();
+            clearFields();
+        }
+    }
+
+    private void loadUsers() {
+        List<UserModel> userModelList = UserRepository.getAllUsers();
+        userModelObservableList.setAll(userModelList);
+        usersTable.setItems(userModelObservableList);
+    }
+
+    private void clearFields() {
+        usernameTextField.clear();
+        passwordTextField.clear();
+        roleComboBox.getSelectionModel().clearSelection();
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
