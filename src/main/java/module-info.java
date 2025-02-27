8cbe8ddba79c33f578066c972ad6451312d323b2
GEAT <gersontajiboy8@gmail.com>
2025-02-26 21:12:18 -0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,4 +17,5 @@ module com.example.demo {
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
     opens com.example.demo.controller to javafx.fxml;
+    opens com.example.demo.models to java.base, javafx.fxml;
 }
